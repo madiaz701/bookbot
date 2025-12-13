86d861ed693dd6275e680e01c00d88bf1824a098
madiaz701 <madiaz701@gmail.com>
2025-12-13 16:43:22 -0600

    Finish step 1: read book text

---
+++ main.py
@@ -0,0 +1,15 @@
+def get_book_text(filepath):
+    contents = ""
+    
+    with open(filepath) as f:
+        contents = f.read()
+    
+    return contents
+
+def main():
+    
+    book_path = "books/frankenstein.txt"
+    book_contents = get_book_text(book_path)
+    print(book_contents)
+
+main()
