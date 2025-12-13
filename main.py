b9d247e76a9315ac74c92e60afdbe309cc8f60d0
madiaz701 <madiaz701@gmail.com>
2025-12-13 16:49:24 -0600

    feat: count total words in book and print summary

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ def get_book_text(filepath):
     
     return contents
 
+def count_words(text):
+    list_of_words = text.split()
+    counter = 0
+    for word in list_of_words:
+        counter += 1
+    return counter
+
+
 def main():
     
     book_path = "books/frankenstein.txt"
     book_contents = get_book_text(book_path)
-    print(book_contents)
+    word_count = count_words(book_contents)
+    print(f"Found {word_count} total words")
 
 main()
